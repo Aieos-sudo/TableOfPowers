14fee1a1f14a4f9354bb96ed6078a43a03c622d8
Aieos <firered9000@yahoo.com>
2021-11-29 21:48:22 -0600

    implemented code review suggestions

--- TableOfPowers/Program.cs
+++ TableOfPowers/Program.cs
@@ -4,7 +4,6 @@ namespace TableOfPowers
 {
     internal class Program
     {
-        //This is never called
         static void Title()
         {
             Console.WriteLine("Table of Powers\n");
@@ -13,8 +12,7 @@ namespace TableOfPowers
         {
             string startNumber;
             string stopNumber;
-            //Your approach to finding checking if stopNumber is less than startNumber is fine but an alternative would be to use a do while loop with an if statement to check stopNumber < startNumber. Example below
-            /*
+            Title();
             do
             {
                 Console.Write("Start number: ");
@@ -28,54 +26,33 @@ namespace TableOfPowers
                 }
             }
             while (Convert.ToInt32(stopNumber) < Convert.ToInt32(startNumber));
-            */
-
-            Console.Write("Start number: ");
-            startNumber = Console.ReadLine();
-            Console.Write("Stop number: ");
-            stopNumber = Console.ReadLine();
-            Console.WriteLine();
-            while (Convert.ToInt32(stopNumber) < Convert.ToInt32(startNumber))
-            {
-                Console.WriteLine("Error: Stop number must be greater than Start number");
-                Console.Write("Start number: ");
-                startNumber = Console.ReadLine();
-                Console.Write("Stop number: ");
-                stopNumber = Console.ReadLine();
-                Console.WriteLine();
-            }
             Console.WriteLine("Number        Squared        Cubed");
             Console.WriteLine("======        =======        =====");
             for (int i = Convert.ToInt32(startNumber); i <= Convert.ToInt32(stopNumber); i++)
             {
-                double increment = numberIncrementCalculation(i);
-                double square = powerCalculationSquare(i);
-                double cube = powerCalculationCube(i);
+                double increment = ConvertIntToDouble(i);
+                double square = PowerCalculationSquare(i);
+                double cube = PowerCalculationCube(i);
                 Console.Write(increment);
                 Console.Write("            " + square);
                 Console.Write("           " + cube);
                 Console.WriteLine(  );
             }
-
-
         }
-        //Would rename this to ConvertIntToDouble(int i) to better represent what the function is doing
-        static double numberIncrementCalculation(int i)
+        static double ConvertIntToDouble(int i)
         {
             double increment;
             increment = Convert.ToDouble(i);
             return increment;
         }
-        //Capitialize first letter of method
-        static double powerCalculationSquare(int i)
+        static double PowerCalculationSquare(int i)
         {
             double square;
             square = Math.Pow(Convert.ToInt32(i), 2);
             return square;
 
         }
-        //Capitialize first letter of method
-        static double powerCalculationCube(int i)
+        static double PowerCalculationCube(int i)
         {
             double cube;
             cube = Math.Pow(Convert.ToInt32(i), 3);
